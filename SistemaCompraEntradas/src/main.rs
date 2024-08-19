5edf9055ff81fe87c6bf522d793fc3145f3c5eec
SMatey <casamarosebas@gmail.com>
2024-08-19 00:40:11 -0600

    CreacionEstructuras

--- SistemaCompraEntradas/src/main.rs
+++ SistemaCompraEntradas/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+//=========================================CREATED OF NECESSARY STRUCTURES=========================================
+enum EstadoSilla {
+    Disponible,
+    Reservada,
+    Comprada,
+}
+//-------------------------------------------------
+struct Silla {
+    numero: u32,
+    estado: EstadoSilla,
+}
+//-------------------------------------------------
+struct  Zona {
+    nombre: String,
+    filas: HashMap<u32, Vec<Silla>>
+}
+//-------------------------------------------------
+struct Categoria {
+    nombre: String,
+    zonas: HashMap<String, Zona>,
+}   
+//-------------------------------------------------
+struct Estadio {
+    categorias: Categoria,
+}
+
 fn main() {
-    print!("Hola Parce");
+    print!("Codigo Realizado Con Exito");
 }
